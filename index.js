686680d994d093d1eaeb14611a42d1a9e6df4f91
AnnekathrinvH <annevonhacht@web.de>
2018-07-14 10:55:22 +0200

    added edit and new function

--- index.js
+++ index.js
@@ -9,13 +9,16 @@
     });
 
     var filteredDataArray = [];
+    var singleCustomerArray = [];
 
 
     var Router = Backbone.Router.extend({
         routes: {
             '': 'main',
             'main': 'main',
-            'main/:id': 'customer'
+            'main/:id': 'customer',
+            'edit/:id': 'edit',
+            'new': 'new'
         },
         main: function() {
             var mainCollection = new MainCollection();
@@ -31,33 +34,21 @@
                 el: '#main',
                 id: id
             })
-        }
-    });
-
-
-
-    var CustomerView = Backbone.View.extend({
-        render: function() {
-            var mainPage = $('#detailPage').html();
-            this.$el.html(mainPage);
-
-            var singleCustomerArray = [];
-            var id = this.id;
-            for (var i = 0; i < filteredDataArray.length; i++) {
-                if (filteredDataArray[i]._id === id) {
-                    singleCustomerArray.push(filteredDataArray[i]);
-                }
-            }
-            console.log(singleCustomerArray);
-
-            var renderedData = Handlebars.templates.singleCustomerData(singleCustomerArray);
-            $('#dataContainer2').html(renderedData);
         },
-        initialize: function() {
-            $('#main').empty();
-            this.render();
+        edit: function() {
+            var router = this;
+
+            var editView = new EditView({
+                el: '#main',
+            })
+        },
+        'new': function() {
+            var newView = new NewView({
+                el: '#main',
+            })
         }
     });
+
 
 
 
@@ -85,15 +76,110 @@
                     $('#dataContainer').html(renderedData);
                 }
             });
-
         },
         initialize: function() {
             console.log('initialize');
             $('#main').empty();
             this.render();
         },
-
+        events: {
+            'click #newCustomerButton': function(event) {
+                window.location.hash = 'new';
+            }
+        }
     });
+
+    var NewView = Backbone.View.extend({
+        render: function() {
+            var mainPage = $('#newCustomerPage').html();
+            this.$el.html(mainPage);
+
+        },
+        initialize: function() {
+            $('#main').empty();
+            this.render();
+        },
+        events: {
+            'click .submitButton': function(event) {
+                var newCustomer = {
+                    'company': $('#company').val(),
+                    '_id': $('#id').val(),
+                    'industry': $('#industry').val(),
+                    'about': $('#about').val()
+                };
+
+                filteredDataArray.push(newCustomer);
+
+                console.log(filteredDataArray);
+                window.location.hash = 'main';
+
+            }
+        }
+    });
+
+    var EditView = Backbone.View.extend({
+        render: function() {
+            var mainPage = $('#editPage').html();
+            this.$el.html(mainPage);
+
+            var renderedData = Handlebars.templates.editCustomerData(singleCustomerArray);
+            $('#dataContainer3').html(renderedData);
+        },
+        initialize: function() {
+            $('#main').empty();
+            this.render();
+        },
+        events: {
+            'click .submitButton': function(event) {
+                var newCompany = $('#company').val();
+                var newIndustry = $('#industry').val();
+                var newAbout = $('#about').val();
+                var id = event.currentTarget.id;
+
+                for (var i = 0; i < filteredDataArray.length; i++) {
+                    if (filteredDataArray[i]._id === id) {
+                        filteredDataArray[i].company = newCompany;
+                        filteredDataArray[i].industry = newIndustry;
+                        filteredDataArray[i].about = newAbout;
+                    }
+
+                }
+                console.log(filteredDataArray);
+                window.location.hash = 'main';
+
+            }
+        }
+    });
+
+    var CustomerView = Backbone.View.extend({
+        render: function() {
+            var mainPage = $('#detailPage').html();
+            this.$el.html(mainPage);
+
+            singleCustomerArray = [];
+            var id = this.id;
+            for (var i = 0; i < filteredDataArray.length; i++) {
+                if (filteredDataArray[i]._id === id) {
+                    singleCustomerArray.push(filteredDataArray[i]);
+                }
+            }
+            console.log(singleCustomerArray);
+            var renderedData = Handlebars.templates.singleCustomerData(singleCustomerArray);
+            $('#dataContainer2').html(renderedData);
+        },
+        initialize: function() {
+            $('#main').empty();
+            this.render();
+        },
+        events: {
+            'click .editButton': function(event) {
+                var id = event.currentTarget.id;
+                console.log(id);
+                window.location.hash = 'edit/'+ id;
+            }
+        }
+    });
+
 
     var router = new Router();
     Backbone.history.start();
